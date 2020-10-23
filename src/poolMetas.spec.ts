47d0c5b0a2f992679e30fa2dad5a5679c75154e1
Jenny Pollack <jennypollack3@gmail.com>
2020-10-23 15:21:55 +0200

    update pool info

--- src/poolMetas.spec.ts
+++ src/poolMetas.spec.ts
@@ -1,16 +1,20 @@
 import { poolMetaByShelf } from './poolMetas'
 
 describe('poolMeta', () => {
-  test('should retreive poolMeta by shelf', () => {
+  test('should retrieve poolMeta by shelf', () => {
     expect(
       poolMetaByShelf.get("0x454c86ba7e0cbd959cfa76aa2db799f9d7a816e4")
     ).toStrictEqual({
+      shortName: 'ConsolFreight 1',
       id: '0xf8b4ef7781ba8e1b3df6370f71d526d00aad1ee2',
       shelf: '0x454c86ba7e0cbd959cfa76aa2db799f9d7a816e4',
       pile: '0x95b74ef13ff280a89ce3d7bbefc822c210e9939f',
       nftFeed: '0xab351d3e54c975bfa0c2edafb6fab03f94762111',
       assessor: '0x1aba642c1aac9f8da36f7df73eda4ca73e054084',
-      senior: '0x4c1bfb4e3ecbd6200358038e3f560ab6dee9bcb6',
+      seniorTranche: '0x4c1bfb4e3ecbd6200358038e3f560ab6dee9bcb6',
+      juniorTranche: '0xd5c9a6f250fe21d1e39d4a74e0fa3b69192bd889',
+      drop: '0xd5a8c4ba74438aaacf108095e7697f1d295266d8',
+      tin: '0xcebba0db0834610ea34d5e4ccca62df3382b774a',
       networkId: 'mainnet',
       startBlock: 10002000,
       version: 2
