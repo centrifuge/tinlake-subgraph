c776e8635dad280ec842543b1688fdea91509f3b
Jenny Pollack <jennypollack3@gmail.com>
2020-11-06 19:52:44 +0100

    handleBlock optimization and update day aggregates

--- src/dateUtil.ts
+++ src/dateUtil.ts
@@ -1,8 +1,14 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { Day } from "../generated/schema"
 
+const secondsInDay = 86400
+
 export function createDay(dateString: string): Day {
     let day = new Day(dateString)
+    day.reserve = BigInt.fromI32(0)
+    day.totalDebt = BigInt.fromI32(0)
+    day.assetValue = BigInt.fromI32(0)
+    day.seniorDebt = BigInt.fromI32(0)
     day.save()
     return day;
 }
@@ -12,6 +18,6 @@ export function toBigInt(integer: i32): BigInt {
 }
 
 export function timestampToDate(timestamp: BigInt): BigInt {
-    let daysSinceEpochStart: BigInt = timestamp / BigInt.fromI32(86400);
-    return daysSinceEpochStart * BigInt.fromI32(86400);
+    let daysSinceEpochStart: BigInt = timestamp / BigInt.fromI32(secondsInDay);
+    return daysSinceEpochStart * BigInt.fromI32(secondsInDay);
 }
