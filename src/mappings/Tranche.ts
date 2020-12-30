6893504dd43479f8b4a8a9402d958d7436ecbb40
Jenny Pollack <jennypollack3@gmail.com>
2020-12-30 08:55:58 +0100

    token address instead of tranche address on supply order

--- src/mappings/Tranche.ts
+++ src/mappings/Tranche.ts
@@ -1,16 +1,21 @@
 import { log, dataSource } from '@graphprotocol/graph-ts'
 import { SupplyOrderCall } from '../../generated/templates/Tranche/Tranche'
-import { Account } from '../../generated/schema'
-import { addToGlobalAccounts, createAccount, isSystemAccount, loadOrCreateGlobalAccounts } from '../domain/Account'
+import { Account, PoolAddresses } from '../../generated/schema'
+import { addToGlobalAccounts, createAccount, isSystemAccount } from '../domain/Account'
 import { loadOrCreateTokenBalance } from '../domain/TokenBalance'
 import { loadOrCreateToken } from '../domain/Token'
 import { push } from '../util/array'
 
 // the supply order is the first contact an investor has with tinlake
 export function handleSupplyOrder(call: SupplyOrderCall): void {
-  let token = call.to.toHex()
+  let tranche = call.to.toHex()
   let poolId = dataSource.context().getString('id')
-  log.debug('handle supply order for token {}', [token.toString()])
+  log.debug('handle supply order for tranche {}', [tranche.toString()])
+  let tokenAddresses = PoolAddresses.load(poolId)
+  let token = tokenAddresses.juniorToken
+  if (tokenAddresses.seniorTranche == tranche) {
+    token = tokenAddresses.seniorToken
+  }
 
   let account = call.inputs.usr.toHex()
   let amount = call.inputs.newSupplyAmount
