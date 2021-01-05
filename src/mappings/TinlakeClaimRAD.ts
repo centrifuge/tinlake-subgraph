edcd2cc3ae9d4b582bbb291a880e35ea19b08653
Jenny Pollack <jennypollack3@gmail.com>
2021-01-05 17:10:38 +0100

    fix for claim events

--- src/mappings/TinlakeClaimRAD.ts
+++ src/mappings/TinlakeClaimRAD.ts
@@ -1,5 +1,5 @@
 import { log, BigDecimal } from '@graphprotocol/graph-ts'
-import { Claimed } from '../../generated/templates/Claim/TinlakeClaimRAD'
+import { Claimed } from '../../generated/Claim/TinlakeClaimRAD'
 import { loadOrCreateRewardLink } from '../domain/RewardLink'
 import { loadOrCreateRewardBalance } from '../domain/Reward'
 import { pushUnique } from '../util/array'
