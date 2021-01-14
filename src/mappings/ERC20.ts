e66c4a5bb66f4a5a3b3edb1d2a24b9f5ed327f56
Jenny Pollack <jennypollack3@gmail.com>
2021-01-14 08:04:09 +0100

    lint

--- src/mappings/ERC20.ts
+++ src/mappings/ERC20.ts
@@ -10,12 +10,14 @@ import { pushUnique } from '../util/array'
 export function handleERC20Transfer(event: TransferEvent): void {
   let tokenAddress = dataSource.context().getString('tokenAddress')
   let poolId = dataSource.context().getString('id')
+
   log.debug('handleERC20Transfer: token {}, from {}, to {}, amount {}', [
     tokenAddress,
     event.params.src.toHex(),
     event.params.dst.toHex(),
     event.params.wad.toString(),
   ])
+
   let token = loadOrCreateToken(tokenAddress)
   let src = event.params.src.toHex()
   let dst = event.params.dst.toHex()
