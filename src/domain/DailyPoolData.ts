b491c4b3eb0fad8d51bc4ad6f6be6d8d66d064a0
Avinash Ega <avinash.ega@gmail.com>
2021-10-12 15:00:25 +0200

    Add yield data to daily pool data to show historic yields

--- src/domain/DailyPoolData.ts
+++ src/domain/DailyPoolData.ts
@@ -23,7 +23,7 @@ export function createDailySnapshot(block: ethereum.Block): void {
     let addresses = PoolAddresses.load(pool.id)
     log.info('createDailySnapshot: loaded pool {}', [pool.shortName])
 
-    let dailyPoolData = createDailyPoolData(pool, yesterday.id)
+    let dailyPoolData = createDailyPoolData(pool.id, yesterday.id)
     setDailyPoolValues(pool, dailyPoolData)
 
     let juniorToken = loadOrCreateToken(addresses.juniorToken)
@@ -45,14 +45,10 @@ export function createDailySnapshot(block: ethereum.Block): void {
   resetActiveInvestments()
 }
 
-export function createDailyPoolData(pool: Pool, yesterday: string): DailyPoolData {
-  let dailyPoolData = new DailyPoolData(pool.id.concat(yesterday))
+export function createDailyPoolData(poolId: string, yesterday: string): DailyPoolData {
+  let dailyPoolData = new DailyPoolData(poolId.concat(yesterday))
   dailyPoolData.day = yesterday
-  dailyPoolData.pool = pool.id
-  dailyPoolData.juniorYield30Days = pool.juniorYield30Days
-  dailyPoolData.juniorYield90Days = pool.juniorYield90Days
-  dailyPoolData.seniorYield30Days = pool.seniorYield30Days
-  dailyPoolData.seniorYield90Days = pool.seniorYield90Days
+  dailyPoolData.pool = poolId
   dailyPoolData.reserve = BigInt.fromI32(0)
   dailyPoolData.totalDebt = BigInt.fromI32(0)
   dailyPoolData.assetValue = BigInt.fromI32(0)
@@ -82,6 +78,10 @@ export function setDailyPoolValues(pool: Pool, dailyPoolData: DailyPoolData): vo
   dailyPoolData.currentJuniorRatio = <BigInt>pool.currentJuniorRatio
   dailyPoolData.juniorTokenPrice = <BigInt>pool.juniorTokenPrice
   dailyPoolData.seniorTokenPrice = <BigInt>pool.seniorTokenPrice
+  dailyPoolData.juniorYield30Days = pool.juniorYield30Days
+  dailyPoolData.juniorYield90Days = pool.juniorYield90Days
+  dailyPoolData.seniorYield30Days = pool.seniorYield30Days
+  dailyPoolData.seniorYield90Days = pool.seniorYield90Days
   dailyPoolData.save()
 }
 
