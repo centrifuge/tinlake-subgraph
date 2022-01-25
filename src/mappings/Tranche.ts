468366b6ff6a33c1a31c485611a31b0d14d16b31
Adam Stockermans <adamstox@protonmail.com>
2022-01-25 10:26:42 -0500

    Remove debug logs

--- src/mappings/Tranche.ts
+++ src/mappings/Tranche.ts
@@ -18,15 +18,15 @@ export function handleSupplyOrder(call: SupplyOrderCall): void {
     tranche.toString(),
     account,
   ])
-  let pool = Pool.load(poolId);
+  let pool = Pool.load(poolId)
   let poolAddresses = PoolAddresses.load(poolId)
   let token = poolAddresses.juniorToken
-  let trancheString = "JUNIOR";
-  let tokenPrice = pool.juniorTokenPrice;
+  let trancheString = 'JUNIOR'
+  let tokenPrice = pool.juniorTokenPrice
   if (poolAddresses.seniorTranche == tranche) {
     token = poolAddresses.seniorToken
-    trancheString = "SENIOR";
-    tokenPrice = pool.seniorTokenPrice;
+    trancheString = 'SENIOR'
+    tokenPrice = pool.seniorTokenPrice
   }
 
   // protection from adding system account to internal tracking
@@ -45,33 +45,39 @@ export function handleSupplyOrder(call: SupplyOrderCall): void {
   let tb = loadOrCreateTokenBalance(account, token)
   calculateDisburse(tb, <PoolAddresses>poolAddresses)
 
-  let type = "INVEST_ORDER";
+  let type = 'INVEST_ORDER'
   if (call.inputs.newSupplyAmount.equals(BigInt.fromI32(0))) {
-    type = "INVEST_CANCEL";
+    type = 'INVEST_CANCEL'
   }
 
-  let symbol = Token.load(token) ? Token.load(token).symbol : "-";
+  let symbol = Token.load(token) ? Token.load(token).symbol : '-'
 
-  let id = call.transaction.hash.toHex().concat(account).concat(trancheString).concat(type);
-  log.info('AddInvestorTransaction: id {}, block{}', [id, call.block.number.toString()]);
-  let investorTx = new InvestorTransaction(id);
-  investorTx.owner = account;
-  investorTx.pool = poolId;
-  investorTx.timestamp = call.block.timestamp;
-  investorTx.type = type;
-  investorTx.tokenAmount = tokenPrice.gt(BigInt.fromI32(0)) ? tb.pendingSupplyCurrency.div(tokenPrice.div(fixed27)) : tb.pendingSupplyCurrency;
-  investorTx.currencyAmount = tb.pendingSupplyCurrency;
-  investorTx.gasUsed = call.transaction.gasUsed;
-  investorTx.gasPrice = call.transaction.gasPrice;
-  investorTx.tokenPrice = tokenPrice;
-  investorTx.symbol = symbol;
-  investorTx.newBalance = tb.totalAmount;
-  investorTx.newBalanceValue = tb.totalValue;
-  investorTx.transaction = call.transaction.hash.toHex();
-  investorTx.save();
-  let previousTokenTransaction = loadOrCreatePreviousTransaction(account.concat(token));
-  previousTokenTransaction.prevTransaction = id;
-  previousTokenTransaction.save();
+  let id = call.transaction.hash
+    .toHex()
+    .concat(account)
+    .concat(trancheString)
+    .concat(type)
+  log.info('AddInvestorTransaction: id {}, block{}', [id, call.block.number.toString()])
+  let investorTx = new InvestorTransaction(id)
+  investorTx.owner = account
+  investorTx.pool = poolId
+  investorTx.timestamp = call.block.timestamp
+  investorTx.type = type
+  investorTx.tokenAmount = tokenPrice.gt(BigInt.fromI32(0))
+    ? tb.pendingSupplyCurrency.div(tokenPrice).div(fixed27)
+    : tb.pendingSupplyCurrency
+  investorTx.currencyAmount = tb.pendingSupplyCurrency
+  investorTx.gasUsed = call.transaction.gasUsed
+  investorTx.gasPrice = call.transaction.gasPrice
+  investorTx.tokenPrice = tokenPrice
+  investorTx.symbol = symbol
+  investorTx.newBalance = tb.totalAmount
+  investorTx.newBalanceValue = tb.totalValue
+  investorTx.transaction = call.transaction.hash.toHex()
+  investorTx.save()
+  let previousTokenTransaction = loadOrCreatePreviousTransaction(account.concat(token))
+  previousTokenTransaction.prevTransaction = id
+  previousTokenTransaction.save()
 }
 
 // redemptions shouldn't count towards balance that users get for rewards
@@ -84,15 +90,15 @@ export function handleRedeemOrder(call: RedeemOrderCall): void {
     tranche.toString(),
     account,
   ])
-  let pool = Pool.load(poolId);
+  let pool = Pool.load(poolId)
   let poolAddresses = PoolAddresses.load(poolId)
   let token = poolAddresses.juniorToken
-  let trancheString = "JUNIOR";
-  let tokenPrice = pool.juniorTokenPrice;
+  let trancheString = 'JUNIOR'
+  let tokenPrice = pool.juniorTokenPrice
   if (poolAddresses.seniorTranche == tranche) {
     token = poolAddresses.seniorToken
-    trancheString = "SENIOR";
-    tokenPrice = pool.seniorTokenPrice;
+    trancheString = 'SENIOR'
+    tokenPrice = pool.seniorTokenPrice
   }
 
   // protection from adding system account to internal tracking
@@ -112,31 +118,37 @@ export function handleRedeemOrder(call: RedeemOrderCall): void {
   let tb = loadOrCreateTokenBalance(account, token)
   calculateDisburse(tb, <PoolAddresses>poolAddresses)
 
-  let type = "REDEEM_ORDER";
+  let type = 'REDEEM_ORDER'
   if (call.inputs.newRedeemAmount.equals(BigInt.fromI32(0))) {
-    type = "REDEEM_CANCEL";
+    type = 'REDEEM_CANCEL'
   }
 
-  let symbol = Token.load(token) ? Token.load(token).symbol : "-";
+  let symbol = Token.load(token) ? Token.load(token).symbol : '-'
 
-  let id = call.transaction.hash.toHex().concat(account).concat(trancheString).concat(type);
-  log.info('AddInvestorTransaction: id {}, block{}', [id, call.block.number.toString()]);
-  let investorTx = new InvestorTransaction(id);
-  investorTx.owner = account;
-  investorTx.pool = poolId;
-  investorTx.timestamp = call.block.timestamp;
-  investorTx.type = type;
-  investorTx.tokenAmount = tb.pendingRedeemToken;
-  investorTx.currencyAmount = tokenPrice.gt(BigInt.fromI32(0)) ? tb.pendingRedeemToken.times(tokenPrice.div(fixed27)) : tb.pendingRedeemToken;
-  investorTx.gasUsed = call.transaction.gasUsed;
-  investorTx.gasPrice = call.transaction.gasPrice;
-  investorTx.tokenPrice = tokenPrice;
-  investorTx.symbol = symbol;
-  investorTx.newBalance = tb.totalAmount;
-  investorTx.newBalanceValue = tb.totalValue;
-  investorTx.transaction = call.transaction.hash.toHex();
-  investorTx.save();
-  let previousTokenTransaction = loadOrCreatePreviousTransaction(account.concat(token));
-  previousTokenTransaction.prevTransaction = id;
-  previousTokenTransaction.save();
+  let id = call.transaction.hash
+    .toHex()
+    .concat(account)
+    .concat(trancheString)
+    .concat(type)
+  log.info('AddInvestorTransaction: id {}, block{}', [id, call.block.number.toString()])
+  let investorTx = new InvestorTransaction(id)
+  investorTx.owner = account
+  investorTx.pool = poolId
+  investorTx.timestamp = call.block.timestamp
+  investorTx.type = type
+  investorTx.tokenAmount = tb.pendingRedeemToken
+  investorTx.currencyAmount = tokenPrice.gt(BigInt.fromI32(0))
+    ? tb.pendingRedeemToken.times(tokenPrice).div(fixed27)
+    : tb.pendingRedeemToken
+  investorTx.gasUsed = call.transaction.gasUsed
+  investorTx.gasPrice = call.transaction.gasPrice
+  investorTx.tokenPrice = tokenPrice
+  investorTx.symbol = symbol
+  investorTx.newBalance = tb.totalAmount
+  investorTx.newBalanceValue = tb.totalValue
+  investorTx.transaction = call.transaction.hash.toHex()
+  investorTx.save()
+  let previousTokenTransaction = loadOrCreatePreviousTransaction(account.concat(token))
+  previousTokenTransaction.prevTransaction = id
+  previousTokenTransaction.save()
 }
