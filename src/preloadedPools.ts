0525ce9cddb61677a2df95b9a642d4236c6c6ab2
Adam Stockermans <adamstox@protonmail.com>
2022-02-03 16:50:52 -0500

    New IPFS hash for HTC2 override with ALL affected contracts

--- src/preloadedPools.ts
+++ src/preloadedPools.ts
@@ -23,7 +23,7 @@ export let mainnetPreloadedPools: PreloadedPool[] = [
   },
   {
     id: '0x4ca805ce8ece2e63ffc1f9f8f2731d3f48df89df', // HTC2 new coordinator April 2021
-    ipfsHash: 'QmZBa62V6s4AMbLA3katcMKCXXRS6Kku2GSARZAauUVSDG',
+    ipfsHash: 'QmYBV6TvKYXVoBdW2kBm28RVgbbfJv4KEXHWsHc7ZKB6fr',
     assessorStartBlock: 12342687,
     network: 'mainnet',
   },
