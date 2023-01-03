ce17a9b72c4c28c1524db9d32f4af66d6a866bea
Adam Stox <adamstox@protonmail.com>
2023-01-03 16:15:03 -0500

    fix loan closed attribute, since Ints are non-nullable types in assembly script, use 0 instead of null

--- src/domain/Loan.ts
+++ src/domain/Loan.ts
@@ -23,7 +23,7 @@ export function updateLoans(pool: Pool, pileAddress: string): BigInt[] {
     }
 
     // Ignore closed loans
-    if (loan.closed != null) {
+    if (loan.closed === 0) {
       log.info('updateLoans: loan {} is closed. Will not proceed with update', [loanId])
       return [new BigInt(0), new BigInt(0)]
     }
